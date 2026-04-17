d8202ed446b7033b7c586c1c3a641723598c34eb
fm <haj.maciek@gmail.com>
2026-04-17 13:12:53 +0200

    fix wezterm keys

--- .wezterm.lua
+++ .wezterm.lua
@@ -190,11 +190,59 @@ config.launch_menu = {
 -- ======================
 -- Skróty
 -- ======================
+config.leader = {
+  key = 'a',
+  mods = 'CTRL',
+  timeout_milliseconds = 1000,
+}
+
 config.keys = {
-  { key = "K",     mods = "CMD", action = act.EmitEvent("toggle-focus-mode") },
-  { key = "Enter", mods = "CMD", action = "ToggleFullScreen" },
-  { key = "n",     mods = "CMD", action = "SpawnWindow" },
-  { key = "w",     mods = "CMD", action = act.CloseCurrentTab { confirm = true } },
+  -- fullscreen
+  { key = 'Enter', mods = 'LEADER', action = act.ToggleFullScreen },
+
+  -- tabs
+  { key = 'c', mods = 'LEADER', action = act.SpawnTab 'CurrentPaneDomain' },
+  { key = 'n', mods = 'LEADER', action = act.ActivateTabRelative(1) },
+  { key = 'p', mods = 'LEADER', action = act.ActivateTabRelative(-1) },
+  { key = 'w', mods = 'LEADER', action = act.CloseCurrentTab { confirm = true } },
+
+  -- skok do tabów 1..9
+  { key = '1', mods = 'LEADER', action = act.ActivateTab(0) },
+  { key = '2', mods = 'LEADER', action = act.ActivateTab(1) },
+  { key = '3', mods = 'LEADER', action = act.ActivateTab(2) },
+  { key = '4', mods = 'LEADER', action = act.ActivateTab(3) },
+  { key = '5', mods = 'LEADER', action = act.ActivateTab(4) },
+  { key = '6', mods = 'LEADER', action = act.ActivateTab(5) },
+  { key = '7', mods = 'LEADER', action = act.ActivateTab(6) },
+  { key = '8', mods = 'LEADER', action = act.ActivateTab(7) },
+  { key = '9', mods = 'LEADER', action = act.ActivateTab(8) },
+
+  -- splity jak w tmux
+  { key = '%', mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = '"', mods = 'LEADER|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+
+  -- poruszanie się między panelami
+  { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection 'Left' },
+  { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection 'Up' },
+  { key = 'l', mods = 'LEADER', action = act.ActivatePaneDirection 'Right' },
+
+  -- zmiana rozmiaru paneli
+  { key = 'H', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize { 'Left', 5 } },
+  { key = 'J', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize { 'Down', 5 } },
+  { key = 'K', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize { 'Up', 5 } },
+  { key = 'L', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize { 'Right', 5 } },
+
+  -- zoom / zamykanie pane
+  { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
+  { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true } },
+
+  -- launcher
+  { key = 's', mods = 'LEADER', action = act.ShowLauncher },
+
+  -- kopiowanie jak w terminalach
+  { key = 'c', mods = 'CTRL|SHIFT', action = act.CopyTo 'Clipboard' },
+  { key = 'v', mods = 'CTRL|SHIFT', action = act.PasteFrom 'Clipboard' },
 }
 
 return config
